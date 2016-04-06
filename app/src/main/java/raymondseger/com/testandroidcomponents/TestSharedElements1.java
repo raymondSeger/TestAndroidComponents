c1a00ea137ad79386a2128c956842a14d6614c3d
Raymond Seger <raymondseger@live.com>
2016-04-06 14:41:19 +0700

    test Shared Element Transition
    use makeSceneTransitionAnimation and don't forget to add
    android:transitionName on the XML layout elements

--- app/src/main/java/raymondseger/com/testandroidcomponents/TestSharedElements1.java
+++ app/src/main/java/raymondseger/com/testandroidcomponents/TestSharedElements1.java
@@ -5,9 +5,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class TestSharedElements1 extends AppCompatActivity {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,13 +18,15 @@ public class TestSharedElements1 extends AppCompatActivity {
         setContentView(R.layout.activity_test_shared_elements1);
 
         TextView textView7 = (TextView) findViewById(R.id.textView7);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
         textView7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(TestSharedElements1.this, TestSharedElements2.class);
 
                 // this will create scene transition data
-                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(TestSharedElements1.this);
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(TestSharedElements1.this, imageView, "theImageName");
 
                 startActivity(intent, options.toBundle() );
             }
--- app/src/main/java/raymondseger/com/testandroidcomponents/TestSharedElements2.java
+++ app/src/main/java/raymondseger/com/testandroidcomponents/TestSharedElements2.java
@@ -2,6 +2,7 @@ package raymondseger.com.testandroidcomponents;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Fade;
 import android.transition.Slide;
 import android.view.Gravity;
 
@@ -13,6 +14,9 @@ public class TestSharedElements2 extends AppCompatActivity {
         setContentView(R.layout.activity_test_shared_elements2);
 
         // slide everything when starts
+        /*
         getWindow().setEnterTransition(new Slide(Gravity.RIGHT));
+        getWindow().setReturnTransition(new Fade());
+        */
     }
 }
