be64c22053d2174116aed99f2bd8973689c56bc8
Raymond Seger <raymondseger@live.com>
2016-04-06 14:36:04 +0700

    test SceneTransition, and the second activity will slide from right when user enter
    test SceneTransition, and the second activity will slide from right when
    user enter

---
+++ app/src/main/java/raymondseger/com/testandroidcomponents/TestSharedElements1.java
@@ -0,0 +1,30 @@
+package raymondseger.com.testandroidcomponents;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class TestSharedElements1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_shared_elements1);
+
+        TextView textView7 = (TextView) findViewById(R.id.textView7);
+        textView7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TestSharedElements1.this, TestSharedElements2.class);
+
+                // this will create scene transition data
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(TestSharedElements1.this);
+
+                startActivity(intent, options.toBundle() );
+            }
+        });
+    }
+}
